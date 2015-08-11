c942dfc71e03a8e8b137243637913f1c0ad8a1e4
James Saryerwinnie <js@jamesls.com>
2015-08-10 21:37:51 -0700

    Regen lrtables

--- jmespath/parser.py
+++ jmespath/parser.py
@@ -330,7 +330,7 @@ class Parser(object):
         grammar.tokens = self._lexer_definition.tokens
         parser = ply.yacc.yacc(module=grammar, debug=self._debug,
                                tabmodule=self._table_module,
-                               write_tables=False)
+                               write_tables=True)
         parsed = self._parse_expression(parser=parser, expression=expression,
                                         lexer_obj=lexer)
         parsed_result = ParsedResult(expression, parsed)
